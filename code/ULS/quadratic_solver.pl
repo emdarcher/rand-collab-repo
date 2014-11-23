c24b6682311b5c926061c813c64f17be43addf98
Ethan Durrant <emd519@yahoo.com>
2014-11-23 14:34:18 -0800

    working, with blank input detection

--- code/ULS/quadratic_solver.pl
+++ code/ULS/quadratic_solver.pl
@@ -3,16 +3,19 @@
 use strict;
 use warnings;
 
-print "Quadratic equation solver:\nplease enter the a,b,&c values of ax^2+bx+c = 0\nvalue a: ";
-chomp(my $a=<STDIN>);
+print "Quadratic equation solver:
+please enter the a,b,&c values of ax^2+bx+c = 0 below:\n";
+print "value a: ";
+chomp(my $a=<STDIN>);unless($a){$a=0;}
 print "value b: ";
-chomp(my $b=<STDIN>);
+chomp(my $b=<STDIN>);$b=0 unless $b;
 print "value c: ";
-chomp(my $c=<STDIN>);
+chomp(my $c=<STDIN>);$c=0 unless $c;
 
 #quadratic equation part
 my $root = eval {(sqrt(($b**2)-(4*$a*$c)))}
-    or die "Trying to take square root of a negative number! This won't work with the quadratic equation!";
+    or die "Trying to take square root of a negative number!
+This won't work with the quadratic equation!\n";
 my $x0 = ((0-$b)+$root)/(2*$a);
 my $x1 = ((0-$b)-$root)/(2*$a);
 printf "resulting x0 = %g; x1 = %g;\n", $x0,$x1;

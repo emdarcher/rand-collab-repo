b8f987da9ea2520c0d1fe3a05149155a15e176e5
Ethan Durrant <emd519@yahoo.com>
2014-11-23 14:12:28 -0800

    quadratic solver working as long as there is input for each number

--- code/ULS/quadratic_solver.pl
+++ code/ULS/quadratic_solver.pl
@@ -11,6 +11,8 @@ print "value c: ";
 chomp(my $c=<STDIN>);
 
 #quadratic equation part
-my $x0 = ((0-$b)+sqrt(($b**2)-(4*$a*$c)))/(2*$a);
-my $x1 = ((0-$b)-sqrt(($b**2)-(4*$a*$c)))/(2*$a);
-printf "resulting x0 = %d; x1 = %d;", $x0,$x1;
+my $root = eval {(sqrt(($b**2)-(4*$a*$c)))}
+    or die "Trying to take square root of a negative number! This won't work with the quadratic equation!";
+my $x0 = ((0-$b)+$root)/(2*$a);
+my $x1 = ((0-$b)-$root)/(2*$a);
+printf "resulting x0 = %g; x1 = %g;\n", $x0,$x1;
